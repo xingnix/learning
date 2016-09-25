d5eb350ad562cd72f4280503759425dfb82045ee
xingnix <xingnix@gmail.com>
2016-09-25 18:00:58 +0800

    refined ANN demo

--- machinelearning/python/ann/backpropagation.py
+++ machinelearning/python/ann/backpropagation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def createnetwork(n_in,n_out,n_hidden):
 	return [{"w":np.random.random((n_hidden,n_in+1))-0.5,
@@ -25,18 +26,43 @@ def backpropogation(training_examples,eta,n_in,n_out,n_hidden):
 			L[1]['w'][:,:-1]+=eta*np.outer(L[1]['delta'],L[0]['o'])
 			L[1]['w'][:,-1]+=eta*L[1]['delta']
 			L[0]['w']+=eta*np.outer(L[0]['delta'],x)
-	for (x,t) in training_examples:
-#		print(map(round,L[0]['o']))
-		print(x,t,int(round(forward(L,x)[0])))
-training_examples=[([1,1,1],1),([-1,-1,1],0),([1,-1,1],0),([-1,1,1],0)]
-#backpropogation(training_examples,0.1,2,1,3)
-training_examples=[([1,1,1],0),([-1,-1,1],0),([1,-1,1],1),([-1,1,1],1)]
-#backpropogation(training_examples,0.5,2,1,2)
+                p=precision(training_examples,L)
+		print "iteration: " , n , "  precision: " , np.int(p*100) , "%"
+	        if(p==1):
+			break
 
-a=np.eye(8)
-b=np.ones((8,9))
-b[:,:-1]=a
-training_examples=[]
-for i in range(8):
-    training_examples.append((b[i,:],a[i,:]))
-backpropogation(training_examples,0.5,8,8,3)
+#	for (x,t) in training_examples:
+#		print(map(round,L[0]['o']))
+#		print(x,t,int(round(forward(L,x)[0])))
+
+def precision(training_examples,L):
+	return np.sum(map(lambda xt:np.double(np.abs(forward(L,xt[0])-xt[1])<0.5),training_examples))/len(training_examples)/np.size(training_examples[0][1])
+def run_linear():
+	training_examples=[([1,1,1],1),([-1,-1,1],0),([1,-1,1],0),([-1,1,1],0)]
+	backpropogation(training_examples,0.9,2,1,3)
+def run_xor():
+	training_examples=[([1,1,1],0),([-1,-1,1],0),([1,-1,1],1),([-1,1,1],1)]
+	backpropogation(training_examples,0.9,2,1,2)
+def run_autoencoder():
+	a=np.eye(8)
+	b=np.ones((8,9))
+	b[:,:-1]=a
+	training_examples=[]
+	for i in range(8):
+		training_examples.append((b[i,:],a[i,:]))
+        backpropogation(training_examples,0.5,8,8,3)
+	 
+def usage():
+    print "usage: \n python  backpropogation.py  <arg> \n  arg:  linear, xor, autoencoder"
+
+if len(sys.argv)==2:
+      if sys.argv[1]=='linear' :
+          run_linear()
+      elif sys.argv[1]=='xor':
+          run_xor()
+      elif sys.argv[1]=='autoencoder':
+          run_autoencoder()
+      else:
+          usage()
+else:
+      usage()
--- machinelearning/python/ann/perceptron.py
+++ machinelearning/python/ann/perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def gradient_descent_linear_perceptron(training_examples,eta):
 	length=len(training_examples[0][0])
@@ -11,14 +12,11 @@ def gradient_descent_linear_perceptron(training_examples,eta):
 			e+=(t-o)**2
 			dw+=eta*(t-o)*np.array(x)
 		wnew=w+dw
-		if np.linalg.norm(wnew-w)<0.001 :
+		precision=correct(training_examples,w,'+-')
+		print "precision: ", np.int(precision*100) , "%"
+		if np.linalg.norm(wnew-w)<0.001 or precision==1:
 		   break 
 		w=wnew
-		print(w)
-training_examples=[([1,1,1],1),([-1,-1,1],-1),([1,-1,1],-1),([-1,1,1],-1)]
-#print("gradient descent method for linear perceptron ")
-#gradient_descent_linear_perceptron(training_examples,0.1)
-
 
 
 def gradient_descent_sigmoid(training_examples,eta):
@@ -33,15 +31,11 @@ def gradient_descent_sigmoid(training_examples,eta):
 			dw+=eta*(t-o)*o*(1-o)*np.array(x)
 			# dw+=eta*(t-o)*np.array(x)   #maximum likelihood
 		wnew=w+dw
-		if np.linalg.norm(wnew-w)<0.001 :
+		precision=correct(training_examples,w,'01')
+		print "precision: ", np.int(precision*100) , "%"
+		if np.linalg.norm(wnew-w)<0.001 or precision==1:
 		   break 
 		w=wnew
-		print(w)
-training_examples=[([1,1,1],1),([-1,-1,1],0),([1,-1,1],0),([-1,1,1],0)]
-print("gradient descent method for sigmoid perceptron ")
-gradient_descent_sigmoid(training_examples,0.1)
-
-
 
 def gradient_descent_sigmoid_like(training_examples,eta):
 	length=len(training_examples[0][0])
@@ -55,10 +49,49 @@ def gradient_descent_sigmoid_like(training_examples,eta):
 			e+=(t-o2)**2
 			dw+=eta*(t-o2)*o1*(1-o1)*np.array(x)
 		wnew=w+dw
-		if np.linalg.norm(wnew-w)<0.001 :
+		precision=correct(training_examples,w,'+-')
+		print "precision: ", np.int(precision*100) , "%"
+		if np.linalg.norm(wnew-w)<0.001 or precision==1:
 		   break 
 		w=wnew
-		print(w)
-training_examples=[([1,1,1],1),([-1,-1,1],-1),([1,-1,1],-1),([-1,1,1],-1)]
-#print("gradient descent method for sigmoid like perceptron ")
-#gradient_descent_sigmoid_like(training_examples,0.1)
+		
+
+def correct(training_examples,w,flag):
+	if flag=='+-':
+ 	    return sum(map(lambda xt : 1.0 if (w.dot(xt[0])*xt[1])>0 else 0, 
+  			    training_examples))/len(training_examples)
+	elif flag=='01':
+ 	    return sum(map(lambda xt:1.0 if np.abs(1/(1+np.exp(-w.dot(xt[0])))-xt[1])<0.5 else 0, 
+ 			    training_examples))/len(training_examples)
+		    
+
+
+def run_linear():
+    training_examples=[([1,1,1],1),([-1,-1,1],-1),([1,-1,1],-1),([-1,1,1],-1)]
+    print("gradient descent method for linear perceptron ")
+    gradient_descent_linear_perceptron(training_examples,0.1)
+
+def run_sigmoid():
+    training_examples=[([1,1,1],1),([-1,-1,1],0),([1,-1,1],0),([-1,1,1],0)]
+    print("gradient descent method for sigmoid perceptron ")
+    gradient_descent_sigmoid(training_examples,0.1)
+
+def run_sigmoid_like():
+    training_examples=[([1,1,1],1),([-1,-1,1],-1),([1,-1,1],-1),([-1,1,1],-1)]
+    print("gradient descent method for sigmoid like perceptron ")
+    gradient_descent_sigmoid_like(training_examples,0.1)
+ 
+def usage():
+    print "usage: \n python  perception.py  <arg> \n  arg:  linear, sigmoid, sigmoid_like"
+
+if len(sys.argv)==2:
+      if sys.argv[1]=='linear' :
+          run_linear()
+      elif sys.argv[1]=='sigmoid':
+          run_sigmoid()
+      elif sys.argv[1]=='sigmoid_like':
+          run_sigmoid_like()
+      else:
+          usage()
+else:
+      usage()
