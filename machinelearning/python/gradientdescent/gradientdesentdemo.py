4c0de7dc6b839f416677870e0e99b040490ab942
xingnix <xingnix@gmail.com>
2017-11-22 16:16:19 +0800

    python 2/3 support for gradient descent

--- machinelearning/python/gradientdescent/gradientdesentdemo.py
+++ machinelearning/python/gradientdescent/gradientdesentdemo.py
@@ -1,27 +1,27 @@
 import numpy as np
 class quadraticform:
     def __init__ (self, w, b):
-	self.w=w
-	self.b=b
+        self.w=w
+        self.b=b
     def f(self,x):
-	return np.transpose(x-self.b)*self.w*(x-self.b)
+        return np.transpose(x-self.b)*self.w*(x-self.b)
     def gradient(self,x):
-	return self.w*(x-self.b)
+        return self.w*(x-self.b)
 
 m=quadraticform(np.matrix([[1,0],[0,1]]),np.matrix([[1],[1]]))
 x=np.matrix([[0],[0]]);
 for i in range(1000):
-        print x[:,-1]
-	x=np.hstack((x,x[:,-1]-m.gradient(x[:,-1])*0.1))
-	if np.linalg.norm(x[:,-1]-x[:,-2])<0.0001:
-	     break
+        print(x[:,-1])
+        x=np.hstack((x,x[:,-1]-m.gradient(x[:,-1])*0.1))
+        if np.linalg.norm(x[:,-1]-x[:,-2])<0.0001:
+             break
 
 def surface(x_interval,y_interval,resolution):
-	for i in range(resolution):
-		s=' '
-		for j in range(resolution):
-			x=x_interval[0]+1.0*i*(x_interval[1]-x_interval[0])/resolution
-			y=y_interval[0]+1.0*j*(y_interval[1]-y_interval[0])/resolution
-			s=s+str( m.f(np.matrix([[x],[y]]))[0,0])+' '
-		print s
+        for i in range(resolution):
+                s=' '
+                for j in range(resolution):
+                        x=x_interval[0]+1.0*i*(x_interval[1]-x_interval[0])/resolution
+                        y=y_interval[0]+1.0*j*(y_interval[1]-y_interval[0])/resolution
+                        s=s+str( m.f(np.matrix([[x],[y]]))[0,0])+' '
+                print(s)
 #surface([-1,1],[-1,1],100)
--- machinelearning/python/gradientdescent/linearregression.py
+++ machinelearning/python/gradientdescent/linearregression.py
@@ -3,15 +3,20 @@ import numpy as np
 import os
 import sys
 import re
+if sys.version > '3':
+    from functools import reduce
+    
+
+
 class model:
     def __init__ (self, samples, target):
-	self.samples=samples
-	self.target=target
+        self.samples=samples
+        self.target=target
     def f(self,x):
-	return np.transpose(self.samples*x-self.target)*(self.samples*x-self.target)
+        return np.transpose(self.samples*x-self.target)*(self.samples*x-self.target)
 
     def gradient(self,x):
-	return 2*np.transpose(self.samples)*(self.samples*x-self.target)
+        return 2*np.transpose(self.samples)*(self.samples*x-self.target)
 
 samples=np.matrix([[3,0],[2,0],[0,1],[0.1,1]])
 target=np.matrix([[1],[1],[0],[0]])
@@ -21,11 +26,11 @@ def train(m,samples,target):
     x=np.matrix([[1],[1]]);
     for i in range(1000):
          print(reduce(lambda x ,y :
-			str(x)+" "*(30-len(str(x)))+str(y)+" ",
-		np.array(x)[:,-1]))
-    	 x=np.hstack((x,x[:,-1]-m.gradient(x[:,-1])*0.05))
-     	 if np.linalg.norm(x[:,-1]-x[:,-2])<0.0001:
-     	     break
+                        str(x)+" "*(30-len(str(x)))+str(y)+" ",
+                np.array(x)[:,-1]))
+         x=np.hstack((x,x[:,-1]-m.gradient(x[:,-1])*0.05))
+         if np.linalg.norm(x[:,-1]-x[:,-2])<0.0001:
+             break
     return x
 
 
@@ -36,25 +41,25 @@ def run_train_file(filename='./train_process.txt'):
    f.close()
    os.system("gnuplot -e \"set grid;plot '"+filename+"' using 0:1 w lp , '' using 0:2 w lp ; pause -1\"")
 def surface_line_string(x_interval,y_interval,resolution):
-	for i in range(resolution):
-		s=' '
-		for j in range(resolution):
-			x=x_interval[0]+1.0*i*(x_interval[1]-x_interval[0])/resolution
-			y=y_interval[0]+1.0*j*(y_interval[1]-y_interval[0])/resolution
-			s=s+str( m.f(np.matrix([[x],[y]]))[0,0])+' '
-		print s
+        for i in range(resolution):
+                s=' '
+                for j in range(resolution):
+                        x=x_interval[0]+1.0*i*(x_interval[1]-x_interval[0])/resolution
+                        y=y_interval[0]+1.0*j*(y_interval[1]-y_interval[0])/resolution
+                        s=s+str( m.f(np.matrix([[x],[y]]))[0,0])+' '
+                print(s)
 def error(m,x_interval,y_interval,resolution,output='./error_surface_index.txt'):
-	x,y=np.meshgrid(np.linspace(x_interval[0],x_interval[1],resolution),
-	            np.linspace(y_interval[0],y_interval[1],resolution))
+        x,y=np.meshgrid(np.linspace(x_interval[0],x_interval[1],resolution),
+                    np.linspace(y_interval[0],y_interval[1],resolution))
         z=np.zeros([resolution,resolution])
         f=open(output,'w')
-	for i in range(resolution):
-		for j in range(resolution):
-			z[i,j]= m.f(np.matrix([[x[i,j]],[y[i][j]]]))
+        for i in range(resolution):
+                for j in range(resolution):
+                        z[i,j]= m.f(np.matrix([[x[i,j]],[y[i][j]]]))
                         f.write(str(x[i,j])+" "+str(y[i,j])+" "+str(z[i,j])+"\n")
-	        f.write("\n")
+                f.write("\n")
         f.close()
-	return z
+        return z
 def run_error_file(filename='./error_surface.txt'):
     z=error(m,[-9,9],[-9,9],50)
     f=open(filename,'w')
@@ -70,8 +75,8 @@ def run_trace_file(filename1='./error_surface_index.txt',filename2='./train_proc
                     np.linspace(y_interval[0],y_interval[1],resolution))
     f=open(filename1,'w')
     for i in range(resolution):
-	for j in range(resolution):
-		z= m.f(np.matrix([[x[i,j]],[y[i][j]]]))
+        for j in range(resolution):
+                z= m.f(np.matrix([[x[i,j]],[y[i][j]]]))
                 f.write(str(x[i,j])+" "+str(y[i,j])+" "+str(z[0,0])+"\n")
         f.write("\n")
     f.close()
@@ -84,7 +89,7 @@ def run_trace_file(filename1='./error_surface_index.txt',filename2='./train_proc
     os.system('gnuplot -e \'set contour base;set cntrparam levels incremental  0,0.2,8;splot "'+filename1+'" w pm3d , "'+filename2+'" w lp lt rgb "white";pause -1;\'')
 
 def usage():
-	print("usage:\n python linearregression.py train \n or \n python linearregression.py errorspace")
+        print("usage:\n python linearregression.py train \n or \n python linearregression.py errorspace")
 
 if len(sys.argv)==2:
     if sys.argv[1]=='train' :
@@ -94,6 +99,6 @@ if len(sys.argv)==2:
     elif sys.argv[1]=='trace':
         run_trace_file()
     else:
-	usage()
+        usage()
 else:
     usage()
