eb5ffb64b13d9e1b83c94a5e7e19e753bd20781f
Xing Chao <xingnix@gmail.com>
2021-12-21 00:11:20 +0800

    translate to keras 2 keras_example.py

--- machinelearning/python/ann/keras_example.py
+++ machinelearning/python/ann/keras_example.py
@@ -7,6 +7,6 @@ m=K.models.Model(x,y)
 #m.compile(loss=K.losses.MSE,optimizer=K.optimizers.SGD(lr=0.1,decay=1e-6, momentum=0.9,nesterov=True))
 #m.compile(loss=K.losses.MSE,optimizer=K.optimizers.SGD(lr=0.1,decay=1e-6, momentum=0.9))
 m.compile(loss=K.losses.MSE,optimizer=K.optimizers.SGD(lr=0.1,momentum=0.9))
-m.fit(np.reshape(np.array([0,0,1,1,1,0,0,1]),(4,2)),np.array([0,0,1,1]),batch_size=4,nb_epoch=1000)
+m.fit(np.reshape(np.array([0,0,1,1,1,0,0,1]),(4,2)),np.array([0,0,1,1]),batch_size=4,epochs=1000)
 print(m.predict(np.reshape(np.array([0,0,1,1,1,0,0,1]),(4,2))))
 
